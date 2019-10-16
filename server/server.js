e882963db632b07c0ebda8061b4a5b8975fe4d77
Nam Dinh <namdinh@aloka.tech>
2019-10-16 12:31:42 +0700

    install pakages -02

--- server/server.js
+++ server/server.js
@@ -1,2 +1,13 @@
 // Call package
 
+var jwt = require('jsonwebtoken');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var port = process.env.PORT || 8000; // Set the port to the app
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mean-stack-project');
+
