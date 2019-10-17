f28eb2313fa01b8981ccb0b281f6896a5bf1126f
Nam Dinh <namdinh@aloka.tech>
2019-10-17 10:02:57 +0700

    cxzcx

--- server/server.js
+++ server/server.js
@@ -11,3 +11,38 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/mean-stack-project');
 
+//Get an instace of the express router
+
+var apiRouter = express.Router();
+
+//App configuration
+//User body-parser 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//Configuration app to handle CROS requests
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    next();
+});
+
+
+// Log all requests to the console
+app.use(morgan('dev'));
+
+
+// Declare routes here
+    
+// 
+
+// START THE SERVER
+// ==========
+app.listen(port);
+console.log('Port must be use is: ' + port);
